9b77f424fa304cd76b00aaf152df87f0420442a9
Travis Veazey <travis.veazey@k12northstar.org>
2022-12-23 17:06:14 -0900

    Using jump search instead of binary search

--- src/main.rs
+++ src/main.rs
@@ -54,12 +54,80 @@ fn find_hash<R: BufRead + Seek>(reader: &mut R, hash: &str) -> Option<usize> {
     None
 }
 
+/// Use a jump search to progressively search through the file for sorted hashes
+///
+/// Based on the algorithm described at https://www.geeksforgeeks.org/jump-search/
+fn jump_search<R: BufRead + Seek>(reader: &mut R, hash: &str) -> Option<usize> {
+    let start = reader.stream_position().unwrap();
+    let n = reader.seek(SeekFrom::End(0)).unwrap();
+    let step = ((n as f32).sqrt() as u64).max(1);
+
+    let mut line = String::new();
+
+    reader.seek(SeekFrom::Start(start)).unwrap();
+
+    loop {
+        if reader.read_line(&mut line).unwrap() == 0 {
+            return None; // Reached the end of the file without finding our target
+        }
+
+        let split_line: Vec<_> = line.trim().split(':').collect();
+
+        match split_line[0].cmp(hash) {
+            std::cmp::Ordering::Equal => return Some(split_line[1].parse().unwrap()),
+            std::cmp::Ordering::Less => { },
+            std::cmp::Ordering::Greater => break,
+        }
+
+        reader.seek(SeekFrom::Current(step as i64)).unwrap();
+        reader.read_line(&mut line).unwrap();
+        line.clear();
+    }
+
+    // Found the segment where our hash may be, start looking for it linearly
+    let stop_at = reader.stream_position().unwrap();
+    // Start by backing up to the start of the segment, then read the maybe-partial line
+    reader.seek(SeekFrom::Current(0 - step as i64)).unwrap();
+    reader.read_line(&mut line).unwrap();
+    while reader.stream_position().unwrap() < stop_at {
+        line.clear();
+        reader.read_line(&mut line).unwrap();
+
+        let split_line: Vec<_> = line.trim().split(':').collect();
+
+        match split_line[0].cmp(hash) {
+            std::cmp::Ordering::Equal => return Some(split_line[1].parse().unwrap()),
+            std::cmp::Ordering::Less => { },
+            std::cmp::Ordering::Greater => break, // Overshot our target, means it's not here
+        }
+    }
+
+    None
+}
+
 
 fn main() {
     let start = std::time::Instant::now();
 
     let hashes = File::open("../hash.csv").expect("Unable to open hashes file");
     let hashreader = BufReader::new(hashes);
+
+    let mut total_users = 0;
+    let mut users: Vec<_> = hashreader.lines().flat_map(|line| {
+        let line = line.ok()?;
+        let split: Vec<_> = line.split_whitespace().collect();
+
+        total_users += 1;
+        
+        Some(User {
+            rid: split[0].parse().expect("Failed to parse RID: {line}"),
+            username: split[1].to_string(),
+            password: split[2].to_ascii_uppercase(),
+            uac: split[3].parse().expect("Failed to parse userAccountControl: {line}"),
+        })
+    }).filter(|user| user.is_active()).collect();
+    users.sort_unstable_by(|a, b| a.password.cmp(&b.password));
+    let active_users = users.len();
 
     let file = File::open("../pwned-passwords-ntlm-ordered-by-hash-v8.txt").expect("Unable to open pwned passwords file");
     let mut reader = BufReader::new(file);
@@ -69,33 +137,21 @@ fn main() {
 
     writeln!(&mut writer, "RID\tUser\tuserAccountControl\tPwned").expect("Failed to write to file");
 
-    let mut users = 0;
-    let mut active_users = 0;
     let mut pwned_users = 0;
 
-    for (user, pwned) in hashreader.lines().flatten().filter_map(|line| {
-        // let upper = line.to_ascii_uppercase();
-        let split: Vec<_> = line.split_whitespace().collect();
-        
-        let user = User {
-            rid: split[0].parse().expect("Failed to parse RID: {line}"),
-            username: split[1].to_string(),
-            password: split[2].to_ascii_uppercase(),
-            uac: split[3].parse().expect("Failed to parse userAccountControl: {line}"),
-        };
+    let mut last_hash = String::new();
+    let mut last_pwned = 0;
 
-        users += 1;
-
-        if user.is_active() {
-            active_users += 1;
-            Some(user)
-        } else {
-            None
+    for (user, pwned) in users.iter().filter_map(|user| {
+        if user.password == last_hash {
+            pwned_users += 1;
+            return Some((user, last_pwned));
         }
-    }).filter_map(|user| {
-        let pwned = find_hash(&mut reader, user.password.as_str())?;
+        last_hash = user.password.clone();
+
+        last_pwned = jump_search(&mut reader, user.password.as_str())?;
         pwned_users += 1;
-        Some((user, pwned))
+        Some((user, last_pwned))
     })
     {
         writeln!(&mut writer, "{}\t{}\t{}\t{}", user.rid, user.username, user.uac, pwned).expect("Failed to write to file");
@@ -107,5 +163,5 @@ fn main() {
     let minutes = elapsed.as_secs() / 60;
     let seconds = elapsed.as_secs_f32() - (minutes * 60) as f32;
     println!("Finished in {minutes} minutes {seconds:.2} seconds");
-    println!("{users} users; {active_users} active users; {pwned_users} pwned users");
+    println!("{total_users} users; {active_users} active users; {pwned_users} pwned users");
 }
